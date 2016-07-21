14f09d0843f99fae8ac17486003b4c1c93c43d97
actuallysarah <sphulton@gmail.com>
2016-07-21 09:52:10 -0400

    Work on map
    Changed the way the map page works. Now, instead of changing the
    z-index, display: hidden and display: block are used to hide and show
    gallery information.

--- scripts/map.js
+++ scripts/map.js
@@ -1,11 +1,11 @@
 //"reveals" a div by making it the top layer.
 function revealDiv(divName)
 {
-    document.getElementById(divName).style.zIndex = "3";
+    document.getElementById(divName).style.display = "block";
 }
 
 //hides a div by lowering the z-index.
 function hideDiv(divName)
 {
-    document.getElementById(divName).style.zIndex = "-999";
+    document.getElementById(divName).style.display = "none";
 }
